f6c658219d4629bd84f4a70458a57f35bb344168
Gabriel <devGabrielCosta@gmail.com>
2021-07-10 11:59:17 -0300

    Importações e base para o front-end criadas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import App from './vue/app'
+
+import { BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/src/jquery.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+Vue.use(BootstrapVueIcons)
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App }
+})
